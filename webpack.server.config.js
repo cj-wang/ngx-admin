a0404d18513ef688cc11647324acb722320e77d9
cj-wang <changjiang.wang@optus.com.au>
2019-04-23 17:10:52 +1000

    fix: fix style-loader path in dist/server/main.js
    ERROR in ./dist/server/main.js
    Module not found: Error: Can't resolve '../../style-loader/lib/addStyles.js' in 'C:\Users\cwang\git\ngx-admin\dist\server'
     @ ./dist/server/main.js 96:17-64
     @ ./server/app.module.ts
     @ ./server/main.ts

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -2,7 +2,19 @@ const webpack = require('webpack');
 const WebpackConfigFactory = require('@nestjs/ng-universal')
   .WebpackConfigFactory;
 
-module.exports = WebpackConfigFactory.create(webpack, {
+const config = WebpackConfigFactory.create(webpack, {
   // Nest server for SSR
   server: './server/main.ts'
 });
+
+// Fix style-loader path in dist/server/main.js
+config.module.rules.push({
+  test: /main\.js$/,
+  loader: 'string-replace-loader',
+  options: {
+    search: 'require("!../../style-loader/lib/addStyles.js")',
+    replace: 'require("style-loader/lib/addStyles.js")',
+  }
+});
+
+module.exports = config;
