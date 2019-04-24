23977283b8011726cbbe05197c938d6a13aa3f66
cj-wang <changjiang.wang@optus.com.au>
2019-04-24 13:19:52 +1000

    fix: add ssr missing objects

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AngularUniversalModule } from '@nestjs/ng-universal';
+import { AngularUniversalModule, applyDomino } from '@nestjs/ng-universal';
 import { join } from 'path';
+
+const BROWSER_DIR = join(process.cwd(), 'dist/browser');
+applyDomino(global, join(BROWSER_DIR, 'index.html'));
+
+global['Event'] = null;
+global['window']['screen'] = {};
+global['location'] = {};
+global['CKEDITOR'] = {
+  version: '',
+  on: () => {},
+  getUrl: () => {},
+  domReady: () => {},
+  fireOnce: () => {},
+  fire: () => {},
+  _: {},
+};
 
 @Module({
   imports: [
     AngularUniversalModule.forRoot({
-      viewsPath: join(process.cwd(), 'dist/browser'),
+      viewsPath: BROWSER_DIR,
       bundle: require('../server/main'),
       liveReload: true
     })
